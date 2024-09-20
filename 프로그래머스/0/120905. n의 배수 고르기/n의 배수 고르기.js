47632438c4ac88a67c5aa8de1d96a22a9ae2a24e
hyejinleeee <159586671+hyejinleeee@users.noreply.github.com>
2024-09-20 17:53:42 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -0,0 +1,12 @@
+function solution(n, numlist) {
+   
+    let answer = [];
+    
+    for(let num of numlist){
+        if(num % n === 0){
+            answer.push(num)
+        }
+    }
+    
+    return answer;
+}
