c1984f3c3dc64e0801bcd7073148c87f046133a7
hyejinleeee <159586671+hyejinleeee@users.noreply.github.com>
2024-11-15 14:57:33 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.15 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -0,0 +1,7 @@
+function solution(myString, pat) {
+   const lowerMyString = myString.toLowerCase();
+  const lowerPat = pat.toLowerCase();
+  
+
+  return lowerMyString.includes(lowerPat) ? 1 : 0;
+}
