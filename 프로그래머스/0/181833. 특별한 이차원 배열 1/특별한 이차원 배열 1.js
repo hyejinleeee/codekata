e19d76bd380d2ff0d11fb78c86936d7963496334
hyejinleeee <159586671+hyejinleeee@users.noreply.github.com>
2025-04-05 14:45:19 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.63 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,17 @@
+function solution(n) {
+  const result = [];
+
+  for (let i = 0; i < n; i++) {
+    const row = [];
+    for (let j = 0; j < n; j++) {
+      if (i === j) {
+        row.push(1);
+      } else {
+        row.push(0);
+      }
+    }
+    result.push(row);
+  }
+
+  return result;
+}
